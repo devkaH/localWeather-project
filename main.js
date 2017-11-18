1d295d5589011aba378b846d591bff6f68841b45
kaoula <ben.hammadi.kaoula@gmail.com>
2017-11-18 17:09:21 +0100

    update file

--- main.js
+++ main.js
@@ -1,105 +1,91 @@
 
+navigator.geolocation.getCurrentPosition(function (position) {
+  var Longitude = position.coords.longitude
+  var Latitude = position.coords.latitude
 
-navigator.geolocation.getCurrentPosition(function (position){
-	var Longitude = position.coords.longitude;
-	var Latitude = position.coords.latitude;
-	
 	// ajax GET request to get weather information
 
-	$.ajax({
+  $.ajax({
 
-		url : 'https://fcc-weather-api.glitch.me/api/current?lon='+Longitude+'&lat='+Latitude,
-		type : 'GET',
+    url: 'https://fcc-weather-api.glitch.me/api/current?lon=' + Longitude + '&lat=' + Latitude,
+    type: 'GET',
 
-		success : function(data) {
+    success: function (data) {
+      updateUI(data)
+    },
+    error: function ()		{
+      $('.infoWeather').text('Geolocation is not supported by this browser.')
+    },
+    beforeSend: function () { $('.infoWeather').append('<img class="imgSpinner" src="images/imgSpinner.gif">').css('text-align', 'center') },
 
-
-			updateUI(data);
-		},
-		error : function()
-		{
-			$('.infoWeather').text("Geolocation is not supported by this browser.")  ;
-		},
-		beforeSend: function () { $('.infoWeather').append('<img class="imgSpinner" src="images/imgSpinner.gif">').css("text-align","center"); },
-
-
-		complete: function () { $('.imgSpinner').hide(); }
-	});
+    complete: function () { $('.imgSpinner').hide() }
+  })
 
     // Add data to html page
-	function updateUI(data){
-		
-		var city=data.name ;
-		var state=data.sys.country;
-		var temp=data.main.temp ;
-		var weather=data.weather[0].description ;
-		var time=new Date($.now());
-		$('.dateTime').text(time.toString()).css({"color": "#ecd68b", "font-family": "'Lucida Sans Unicode', 'Lucida Grande', sans-serif","font-size" : "2rem"});
-		
-		$('.location').text(city + "," + state);
-		
-		$('.temp').text(temp + " °C");
-		$('.weather').text(weather);
+  function updateUI (data) {
+    var city = data.name
+    var state = data.sys.country
+    var temp = data.main.temp
+    var weather = data.weather[0].description
+    var time = new Date($.now())
+    $('.dateTime').text(time.toString().slice(0, 25)).css({'color': '#ecd68b', 'font-family': "'Lucida Sans Unicode', 'Lucida Grande', sans-serif", 'font-size': '2rem'})
 
-		switch (weather) {
-			case "scattered clouds":
-			
-				$(".weatherIcon").attr("src","images/scatteredClouds.gif");
-				break;
-			case "sunny":
-				
-				$(".weatherIcon").attr("src","images/sunny.gif");
-				break;
-			case "thunderstorm":
-			
-				$(".weatherIcon").attr("src","images/thunderstorm.gif");
-				break;
-			case "rainy":
-				
-				$(".weatherIcon").attr("src","images/raining.gif");
-				break;
-			case "fews clouds":
-				
-				$(".weatherIcon").attr("src","images/cloudy.gif");
-			break;
-			case "light rain":
-			
-				$(".weatherIcon").attr("src","images/lightRain.gif");
-				break;
+    $('.location').text(city + ',' + state)
 
-			
-		}
+    $('.temp').text(temp + ' °C')
+    $('.weather').text(weather)
+
+    switch (weather) {
+      case 'scattered clouds':
+
+        $('.weatherIcon').attr('src', 'images/scatteredClouds.gif')
+        break
+      case 'sunny':
+
+        $('.weatherIcon').attr('src', 'images/sunny.gif')
+        break
+      case 'thunderstorm':
+
+        $('.weatherIcon').attr('src', 'images/thunderstorm.gif')
+        break
+      case 'rainy':
+
+        $('.weatherIcon').attr('src', 'images/raining.gif')
+        break
+      case 'fews clouds':
+
+        $('.weatherIcon').attr('src', 'images/cloudy.gif')
+        break
+      case 'light rain':
+
+        $('.weatherIcon').attr('src', 'images/lightRain.gif')
+        break
+    }
 
         // Change unity of temperature (°F,°C)
-		$('.temp').on('click', function(){
-
-			if ($(this).hasClass('celcius')) {
-				$('.temp')
+    $('.temp').on('click', function () {
+      if ($(this).hasClass('celcius')) {
+        $('.temp')
 				.removeClass('celcius')
 				.addClass('fahrenheit')
-				.text(setCelcius());
-			} else {
-				$('.temp')
+				.text(setCelcius())
+      } else {
+        $('.temp')
 				.removeClass('fahrenheit')
 				.addClass('celcius')
-				.text(setFahrenheit());
-			}
+				.text(setFahrenheit())
+      }
 
 			// $('.temp').text(setCelcius());
-		});
+    })
 
-		function setCelcius(){
-			var fah = (temp * 1.8) + 32 ;
-			return fah.toFixed(2) + " °F";
-		};
+    function setCelcius () {
+      var fah = (temp * 1.8) + 32
+      return fah.toFixed(2) + ' °F'
+    };
 
-		function setFahrenheit(){
-			return temp + " °C";
-		};
-	}
-
-
-
-
-
-});
+    function setFahrenheit () {
+      return temp + ' °C'
+    };
+  }
+})
